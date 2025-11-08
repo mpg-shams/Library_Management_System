6a15de13d41ec183452157b0f78edb3d5aaf2dd4
hossein2081 <117431730+hossein2081@users.noreply.github.com>
2025-11-08 12:21:49 +0330

    Swagger/index.html

--- LibraryManagementApi/Program.cs
+++ LibraryManagementApi/Program.cs
@@ -23,6 +23,16 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.Use(async (context, next) =>
+    {
+        if (context.Request.Path == "/")
+        {
+            context.Response.Redirect("/swagger/index.html");
+            return;
+        }
+
+        await next();
+    });
 }
 
 app.UseHttpsRedirection();
