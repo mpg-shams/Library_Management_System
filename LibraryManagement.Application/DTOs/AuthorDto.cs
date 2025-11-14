a45d81f47f291207d7dcd1f22125b78e25a6423a
Parsanb <parsanasiri123@gmail.com>
2025-11-14 22:04:39 +0330

    New DTO : AuthorDto

---
+++ LibraryManagement.Application/DTOs/AuthorDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library_Management_System.LibraryManagement.Application.DTOs
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
